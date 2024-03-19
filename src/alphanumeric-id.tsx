0852742499f1041bee18df99277a0bda3e8176a2
Gustavo <gustavopch@gmail.com>
2024-03-19 19:02:31 -0300

    Initial commit

---
+++ src/alphanumeric-id.tsx
@@ -0,0 +1,13 @@
+const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const LENGTH = 20
+
+// Adapted from: https://github.com/invertase/react-native-firebase/blob/6783245e19f81297363fc56a53063c8a053782b8/packages/app/lib/common/id.js
+export const alphanumericId = (): string => {
+  let id = ''
+
+  for (let i = 0; i < LENGTH; i++) {
+    id += CHARS.charAt(Math.floor(Math.random() * CHARS.length))
+  }
+
+  return id
+}
